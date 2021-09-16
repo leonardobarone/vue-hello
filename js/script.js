b87c1379a3daab3703a3aa830785e83d915b9560
leonardobarone <leonardobarone26@gmail.com>
2021-09-16 15:20:15 +0200

    hello vue

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+// Declarative rendering
+var app = new Vue({
+    el: '#root',
+    data: {
+        title: 'Hello Vue!',
+        img: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+    }
+})
+
+
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
