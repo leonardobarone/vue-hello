c1d3459334a3b9427f69806007f387eec0bd8cc3
leonardobarone <leonardobarone26@gmail.com>
2021-09-16 15:53:52 +0200

    provato lista e un method

--- js/script.js
+++ js/script.js
@@ -3,8 +3,16 @@ var app = new Vue({
     el: '#root',
     data: {
         title: 'Hello Vue!',
-        img: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80"
+        img: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+        studenti: ["Gennaro", "Alessandro", "Fabrizio", "Pasquale", "Matteo"]
+    },
+    methods: {
+        inserisci: function(){
+            let nuovoStudente = prompt("inserisci studente");
+            this.studenti.push(nuovoStudente)
+        }
     }
+   
 })
 
 
